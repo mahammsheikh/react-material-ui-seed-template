8a4476319d22d50d4e114de357fd68e5e830c631
Firas Abdullah <106663252+fi5421@users.noreply.github.com>
2024-09-30 15:08:25 +0500

    feat: added oval chip and menu levels in side bar (#2)
    Co-authored-by: fi5421 <firas.abdullah@conradlabs.com>

--- src/menu-items/index.jsx
+++ src/menu-items/index.jsx
@@ -1,11 +1,12 @@
 import samplePage from './sample-page';
 import pages from './pages';
 import utilities from './utilities';
+import support from './support';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-    items: [samplePage, pages, utilities]
+    items: [samplePage, pages, utilities,support]
 };
 
 export default menuItems;
