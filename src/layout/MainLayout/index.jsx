5945c5a6c7d7636a05cc7088e6012de4db740987
fi5421 <firas.abdullah@conradlabs.com>
2024-10-14 12:05:47 +0500

    feat: removed Customization files and references

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -14,7 +14,6 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import HorizontalBar from './HorizontalBar';
 import MainContentStyled from './MainContentStyled';
-import Customization from '../Customization';
 import Loader from 'ui-component/Loader';
 import Breadcrumbs from 'ui-component/extended/Breadcrumbs';
 
@@ -67,7 +66,6 @@ const MainLayout = () => {
                     <Outlet />
                 </Container>
             </MainContentStyled>
-            <Customization/>
         </Box>
     );
 };
