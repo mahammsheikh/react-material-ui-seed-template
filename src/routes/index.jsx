b5d0b5bc948f2d463cfea9e2e59a895691c7ab39
fi5421 <firas.abdullah@conradlabs.com>
2024-10-16 16:27:02 +0500

    chore: renamed maintanence route file

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,9 +3,9 @@ import { createBrowserRouter } from 'react-router-dom';
 // routes
 import MainRoutes from './MainRoutes';
 import LoginRoutes from './LoginRoutes';
-import AuthenticationRoutes from './AuthenticationRoutes';
+import MaintanencePageRoutes from './MaintanencePageRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
-const router = createBrowserRouter([LoginRoutes, AuthenticationRoutes, MainRoutes], { basename: import.meta.env.VITE_APP_BASE_NAME });
+const router = createBrowserRouter([LoginRoutes, MaintanencePageRoutes, MainRoutes], { basename: import.meta.env.VITE_APP_BASE_NAME });
 export default router;
